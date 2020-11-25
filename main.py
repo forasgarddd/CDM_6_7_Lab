522cad16981995916d1e9af1791b265b9234046b
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-25 17:19:52 +0200

    some commits

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from tkinter import *
+from MenuBar import MenuBar
 
 root = Tk()
 root.title("Title test")
 
 text = Text(width = 100, height = 40, bg="white", fg="black", wrap=WORD)
 text.pack()
+"""
+mainmenu = Menu(root)
+root.config(menu=mainmenu)
 
-#root.resizable(0,0)
+filemenu = Menu(mainmenu, tearoff=0)
+filemenu.add_command(label="Open")
+filemenu.add_command(label="New")
+filemenu.add_command(label="Save")
+filemenu.add_command(label="Close")
 
+mainmenu.add_cascade(label="File", menu=filemenu)
+"""
+MenuBar(root)
 root.mainloop()
