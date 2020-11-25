e47c0bb794a0bf58889f2f909f906f6d274ae8d7
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-25 19:24:27 +0200

    Some commit

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 from tkinter import *
-from MenuBar import MenuBar
+from tkinter import filedialog
 
 root = Tk()
 root.title("Title test")
 
-text = Text(width = 100, height = 40, bg="white", fg="black", wrap=WORD)
-text.pack()
-"""
-mainmenu = Menu(root)
-root.config(menu=mainmenu)
+#New file function
+def new_file():
+    my_text.delete("1.0", END)
+    root.title("New File")
 
-filemenu = Menu(mainmenu, tearoff=0)
-filemenu.add_command(label="Open")
-filemenu.add_command(label="New")
-filemenu.add_command(label="Save")
-filemenu.add_command(label="Close")
+#Open file function
+def open_file():
+    my_text.delete("1.0", END)
+    text_file = filedialog.askopenfile(initialdir="C:/Desktop", title="Open File", filetypes=(("Text files", "*.txt"), ("All Files", "*.*")))
 
-mainmenu.add_cascade(label="File", menu=filemenu)
-"""
-MenuBar(root)
+    text_file = open(text_file, "r")
+    stuff = text_file.read()
+
+    my_text.insert(END, stuff)
+    text_file.close()
+
+my_frame = Frame(root)
+my_frame.pack(pady=5)
+
+my_text = Text(my_frame, width=97, height=25, selectbackground="white", selectforeground="black", undo=True)
+my_text.pack()
+
+#Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#Add File menu
+file_menu = Menu(my_menu, tearoff=0)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+#MenuBar(root)
 root.mainloop()
