09cc1a4467cf547aa546f74cebf209c3317b55ef
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-26 21:34:20 +0200

    Task 7 button added

--- Notepad.py
+++ Notepad.py
@@ -15,11 +15,12 @@ class Notepad:
     root.grid_rowconfigure(0, weight=1)
     root.grid_columnconfigure(0, weight=1)
 
-    # adding menu
+    # adding menus
     menuBar = Menu(root)
     fileMenu = Menu(menuBar, tearoff=0)
     countMenu = Menu(menuBar, tearoff=0)
-    Task5Menu = Menu(menuBar, tearoff=0)
+    task5Menu = Menu(menuBar, tearoff=0)
+    task7Menu = Menu(menuBar, tearoff=0)
 
     # adding scrollbar
     scrollBar = Scrollbar(textArea, command=textArea.yview)
@@ -27,7 +28,7 @@ class Notepad:
     scrollBar.pack(side=RIGHT, fill=Y)
     file = None
 
-    # <<<Functions applications>>>
+    # <<<Functions executions>>>
 
     def __init__(self):
 
@@ -60,8 +61,8 @@ class Notepad:
         self.menuBar.add_cascade(label="Count", menu=self.countMenu)
 
         # some functions to count menu
-        self.line_count_menu = Menu(self.countMenu, tearoff=0, postcommand=self.findlinecount)
-        self.word_count_menu = Menu(self.countMenu, tearoff=0, postcommand=self.findwordcount)
+        self.line_count_menu = Menu(self.countMenu, tearoff=0, postcommand=self.find_line_count)
+        self.word_count_menu = Menu(self.countMenu, tearoff=0, postcommand=self.find_word_count)
 
         self.countMenu.add_cascade(label="Line count", menu=self.line_count_menu)
         self.countMenu.add_cascade(label="Word count", menu=self.word_count_menu)
@@ -69,10 +70,13 @@ class Notepad:
         self.line_count_menu.add_command(label="0 Lines", command=None)
         self.word_count_menu.add_command(label="0 Words", command=None)
 
-        # Task5 menu cascade
-        self.menuBar.add_cascade(label="Task 5", menu=self.Task5Menu)
-        self.Task5Menu.add_command(label="Print A->Z", command=self.engalphprint)
+        # task 5 menu cascade and execution
+        self.menuBar.add_cascade(label="Task 5", menu=self.task5Menu)
+        self.task5Menu.add_command(label="Print A->Z", command=self.eng_alph_print)
 
+        # task 7 menu cascade and execution
+        self.menuBar.add_cascade(label="Task 7", menu=self.task7Menu)
+        self.task7Menu.add_command(label="Is 'a' more than 'b'?", command=self.a_more_than_b)
 
         # config menu
         self.root.config(menu=self.menuBar)
@@ -126,25 +130,40 @@ class Notepad:
             file.close()
 
     # to count lines
-    def findlinecount(self):
+    def find_line_count(self):
         if self.textArea.compare("end-1c", "!=", "1.0"):
                 self.line_count_menu.entryconfig(0, label=str(str(int
                                                                   (self.textArea.index('end').split('.')[0]) - 1)
                                                               + " Lines"))
 
     # to count words
-    def findwordcount(self):
+    def find_word_count(self):
         if self.textArea.compare("end-1c", "!=", "1.0"):
                 self.word_count_menu.entryconfig(0, label=str(str(len
                                                                   (self.textArea.get(0.0, END).replace("\n", " ").split(" ")) - 1)
                                                               + " Words"))
 
-    #Task 5 logic
-    def engalphprint(self):
+    # task 5 logic
+    def eng_alph_print(self):
         self.root.title("Task 5")
         self.file = None
         self.textArea.delete(1.0, END)
         self.textArea.insert(1.0, ascii_uppercase)
+
+    # task 7 logic
+    def a_more_than_b(self):
+        self.root.title("Task 7")
+        self.file = None
+        stextarea = self.textArea.get("1.0", END)
+        a_count = stextarea.count("a") + stextarea.count("A")
+        b_count = stextarea.count("b") + stextarea.count("B")
+        self.textArea.delete(1.0, END)
+        if a_count > b_count:
+            self.textArea.insert(1.0, "\n" + stextarea)
+            self.textArea.insert(1.0, "true")
+        else:
+            self.textArea.insert(1.0, "\n" + stextarea)
+            self.textArea.insert(1.0, "false")
 
     # app run function
     def run(self):
