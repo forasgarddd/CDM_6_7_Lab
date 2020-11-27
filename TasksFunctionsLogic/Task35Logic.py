56e3f4bf57f693e070e21eb533cf6969ba02304f
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-11-28 00:38:36 +0200

    Task 49 button added

--- TasksFunctionsLogic/Task35Logic.py
+++ TasksFunctionsLogic/Task35Logic.py
@@ -1,18 +1,20 @@
 from tkinter import *
 
+
 def palindrome_check(self):
+
     self.root.title("Task 35")
     self.file = None
-    stextarea = self.textArea.get(1.0, END).replace("\n", "")
-    lcstextarea = stextarea.lower()
-    rev_stextarea = ''.join(reversed(lcstextarea))
-    if len(lcstextarea.split()) > 1:
+    string_textarea = self.textArea.get(1.0, END).replace("\n", "")
+    lc_string_textarea = string_textarea.lower()
+    rev_stextarea = ''.join(reversed(lc_string_textarea))
+    if len(lc_string_textarea.split()) > 1:
         self.textArea.delete(1.0, END)
         self.textArea.insert(1.0,
-                             "Введений текст не є вірним: " + "'" + stextarea + "'" + "\nНеможливо обробити більше 1 слова")
-    elif rev_stextarea == lcstextarea:
+                             "Введений текст не є вірним: " + "'" + string_textarea + "'" + "\nНеможливо обробити більше 1 слова")
+    elif rev_stextarea == lc_string_textarea:
         self.textArea.delete(1.0, END)
-        self.textArea.insert(1.0, "слово: " + "'" + stextarea + "'" + " є паліндромом")
-    elif rev_stextarea != lcstextarea:
+        self.textArea.insert(1.0, "слово: " + "'" + string_textarea + "'" + " є паліндромом")
+    elif rev_stextarea != lc_string_textarea:
         self.textArea.delete(1.0, END)
-        self.textArea.insert(1.0, "слово: " + "'" + stextarea + "'" + " не є паліндромом")
+        self.textArea.insert(1.0, "слово: " + "'" + string_textarea + "'" + " не є паліндромом")
