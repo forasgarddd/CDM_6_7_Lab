9868201a0a61024ce2dd4f54edceb75fe112f168
Ivan Devitskyi <forbattlenetonly14@gmail.com>
2020-12-10 10:57:14 +0200

    final commit

--- TasksFunctionsLogic/Task49Logic.py
+++ TasksFunctionsLogic/Task49Logic.py
@@ -2,10 +2,13 @@ from tkinter import *
 
 
 def print_uppercase(self):
-
     self.root.title("Task 49")
     self.file = None
     string_textarea = self.textArea.get(1.0, END)
     upper_textarea = string_textarea.upper()
     self.textArea.delete(1.0, END)
     self.textArea.insert(1.0, upper_textarea)
+
+
+
+
